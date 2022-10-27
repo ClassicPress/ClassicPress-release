47b1d6ad288174fe8aee08e3841cb6e573b43b77
mattyrob <mattyrobuk@gmail.com>
2022-10-27 16:41:03 +0100

    Release 1.4.4

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -1001,7 +1001,7 @@ function post_custom( $key = '' ) {
  *
  * @since WP-1.2.0
  *
- * @deprecated WP-6.0.2 Use get_post_meta() to retrieve post meta and render manually.
+ * @deprecated 6.0.2 Use get_post_meta() to retrieve post meta and render manually.
  *
  */
 function the_meta() {
