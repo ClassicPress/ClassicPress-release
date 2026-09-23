fbdaefd2eb412c79bed28e94e269b313fc1ab0b3
mattyrob <mattyrobuk@gmail.com>
2026-09-23 16:23:24 +0100

    Release 2.7.3

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_4fff0fc5';
+		$default_version = 'cp_d09bd054';
 	}
 
 	/**
